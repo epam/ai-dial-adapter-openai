f91c723969f5ee35973359140085193b99d1c015
Anton Dubovik <anton_dubovik@epam.com>
2023-07-04 13:22:05 +0100

    Added usage field to the last chunk in streaming mode

--- universal_api/response.py
+++ universal_api/response.py
@@ -26,19 +26,10 @@ def generate_event_stream(
             if item == {}:
                 choice = {"index": 0, "delta": {}, "finish_reason": "stop"}
 
-                # Extra bit specific for the Universal API
-                statistics = {
-                    "per_model": [
-                        {
-                            "index": 0,
-                            "name": params.model,
-                            **usage.to_dict(),
-                        }
-                    ]
-                }
-
+                # Adding usage to the last chunk.
+                # OpenAI itself leaves this field undefined/null, but we provide meaningful usage.
                 yield wrap_streaming_chunk(
-                    params, {"choices": [choice], "statistics": statistics}
+                    params, {"choices": [choice], "usage": usage.to_dict()}
                 )
 
                 yield "data: [DONE]\n\n"
