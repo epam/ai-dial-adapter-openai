34645a538596e3d2b48720ddf82850345791ed76
Anton Dubovik <anton_dubovik@epam.com>
2023-07-14 11:12:04 +0100

    Increased number of workers

--- debug_app.py
+++ debug_app.py
@@ -1,3 +1,5 @@
+import os
+
 import uvicorn
 
 from utils.args import get_host_port_args
@@ -6,4 +8,5 @@ from utils.init import init
 if __name__ == "__main__":
     init()
     host, port = get_host_port_args()
-    uvicorn.run("app:app", host=host, port=port, workers=1)
+    workers = int(os.environ.get("WEB_CONCURRENCY", 1))
+    uvicorn.run("app:app", host=host, port=port, workers=workers)
