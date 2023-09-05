67c74821c774c31f2ad5c17fb0c30adea43557b8
Anton Dubovik <anton_dubovik@epam.com>
2023-09-05 09:32:38 +0100

    Removed unnecessary dependency from inquirer

--- client/client_vertexai.py
+++ client/client_vertexai.py
@@ -4,8 +4,9 @@ from typing import List
 from langchain.schema import AIMessage, BaseMessage, HumanMessage
 
 from llm.vertex_ai_adapter import VertexAIModel
-from llm.vertex_ai_models import choose_model
+from llm.vertex_ai_models import VertexAIModels
 from universal_api.request import CompletionParameters
+from utils.cli import select_enum
 from utils.env import get_env
 from utils.init import init
 from utils.input import make_input
@@ -15,7 +16,7 @@ from utils.printing import print_ai, print_info
 async def main():
     init()
 
-    model_id = choose_model()
+    model_id = select_enum("Select the model", VertexAIModels)
     model = await VertexAIModel.create(
         location=get_env("DEFAULT_REGION"),
         project_id=get_env("GCP_PROJECT_ID"),
