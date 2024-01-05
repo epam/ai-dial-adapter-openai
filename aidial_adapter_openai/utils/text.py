7b724f2d356f9fba0c335ab8ff253dd702016882
Anton Dubovik <anton_dubovik@epam.com>
2024-01-05 14:38:09 +0000

    fix: review comments

--- aidial_adapter_openai/utils/text.py
+++ aidial_adapter_openai/utils/text.py
@@ -1,14 +1,10 @@
-def get_ordinal_suffix(idx: int) -> str:
-    match idx % 10:
-        case 1:
-            return "st"
-        case 2:
-            return "nd"
-        case 3:
-            return "rd"
-        case _:
-            return "th"
-
-
 def format_ordinal(idx: int) -> str:
     return f"{idx}{get_ordinal_suffix(idx)}"
+
+
+def get_ordinal_suffix(idx: int) -> str:
+    idx = abs(idx)
+    if 11 <= (idx % 100) <= 13:
+        return "th"
+    else:
+        return ["th", "st", "nd", "rd", "th"][min(idx % 10, 4)]
