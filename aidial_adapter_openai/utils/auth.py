f3c3f00fdb1b297d18883febfb2fc54208d6f5c2
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2024-03-06 17:19:38 +0300

    fix: resolve PR comments

--- aidial_adapter_openai/utils/auth.py
+++ aidial_adapter_openai/utils/auth.py
@@ -1,36 +1,51 @@
+import os
 import time
 from typing import Mapping, Optional
 
 from azure.core.credentials import AccessToken
+from azure.core.exceptions import ClientAuthenticationError
 from azure.identity import DefaultAzureCredential
 from fastapi import Request
 from openai import util
 from openai.util import ApiType
 from pydantic import BaseModel
 
+from aidial_adapter_openai.utils.exceptions import HTTPException
+from aidial_adapter_openai.utils.log_config import logger
+
 default_credential = DefaultAzureCredential()
 access_token: AccessToken | None = None
 
-EXPIRATION_WINDOW_IN_SEC = 10
+EXPIRATION_WINDOW_IN_SEC: int = int(
+    os.getenv("ACCESS_TOKEN_EXPIRATION_WINDOW", 10)
+)
+AZURE_OPEN_AI_SCOPE: str = os.getenv(
+    "AZURE_OPEN_AI_SCOPE", "https://cognitiveservices.azure.com/.default"
+)
 
 
 async def get_api_key() -> str:
     now = int(time.time()) - EXPIRATION_WINDOW_IN_SEC
     global access_token
+
     if access_token is None or now > access_token.expires_on:
-        access_token = default_credential.get_token(
-            "https://cognitiveservices.azure.com/.default"
-        )
+        try:
+            access_token = default_credential.get_token(AZURE_OPEN_AI_SCOPE)
+        except ClientAuthenticationError as e:
+            logger.error(
+                f"Default Azure credential failed with the error: {e.message}"
+            )
+            raise HTTPException("Authentication failed", 401, "Unauthorized")
+
     return access_token.token
 
 
 async def get_credentials(request: Request) -> tuple[str, str]:
     api_key = request.headers.get("X-UPSTREAM-KEY")
-    api_type = "azure"
     if api_key is None:
-        api_key = await get_api_key()
-        api_type = "azure_ad"
-    return api_type, api_key
+        return "azure_ad", await get_api_key()
+    else:
+        return "azure", api_key
 
 
 def get_auth_header(api_type: str, api_key: str) -> dict[str, str]:
