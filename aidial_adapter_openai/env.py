ba8abbd3d45b2e74a1f7aad7d7f21ff52907156d
Roman Romanov <roman_romanov2@epam.com>
2024-06-05 11:52:36 +0300

    add string processors abstraction

--- aidial_adapter_openai/env.py
+++ aidial_adapter_openai/env.py
@@ -3,6 +3,10 @@ import os
 from typing import Dict
 
 from aidial_adapter_openai.utils.parsers import parse_deployment_list
+from aidial_adapter_openai.utils.string_processor import (
+    StringProcessor,
+)
+
 
 MODEL_ALIASES: Dict[str, str] = json.loads(os.getenv("MODEL_ALIASES", "{}"))
 API_VERSIONS_MAPPING: Dict[str, str] = json.loads(
@@ -26,4 +30,10 @@ DATABRICKS_DEPLOYMENTS = parse_deployment_list(
 GPT4O_DEPLOYMENTS = parse_deployment_list(os.getenv("GPT4O_DEPLOYMENTS") or "")
 LEGACY_COMPLETIONS_DEPLOYMENTS = parse_deployment_list(
     os.getenv("LEGACY_COMPLETIONS_DEPLOYMENTS") or ""
+)
+DEPLOYMENT_TO_PROMPT_PROCESSOR: Dict[str, StringProcessor] = json.loads(
+    os.getenv("DEPLOYMENT_TO_PROMPT_PROCESSOR", "{}")
+)
+DEPLOYMENT_TO_OUTPUT_PROCESSOR: Dict[str, StringProcessor] = json.loads(
+    os.getenv("DEPLOYMENT_TO_OUTPUT_PROCESSOR", "{}")
 )
