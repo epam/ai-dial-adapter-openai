061e7a0d7d4c6718d54aef0c99283740b1020a83
Anton Dubovik <anton_dubovik@epam.com>
2024-09-23 14:03:28 +0100

    fix: simplified mocking code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,23 @@
+from unittest.mock import patch
+
 import httpx
 import pytest
 import pytest_asyncio
 from httpx import ASGITransport
 
-import aidial_adapter_openai.utils.streaming
 from aidial_adapter_openai.app import app
 
 
 @pytest.fixture
-def eliminate_empty_choices(monkeypatch):
-    monkeypatch.setattr(
-        aidial_adapter_openai.utils.streaming, "eliminate_empty_choices", True
-    )
+def eliminate_empty_choices():
+    with patch(
+        "aidial_adapter_openai.utils.streaming.eliminate_empty_choices", True
+    ):
+        yield
 
 
 @pytest_asyncio.fixture
 async def test_app():
-
     async with httpx.AsyncClient(
         transport=ASGITransport(app=app),  # type: ignore
         base_url="http://test-app.com",
