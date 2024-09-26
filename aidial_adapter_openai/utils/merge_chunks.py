07f37b3ec9999873677023a8b7751b2d5b7a467e
Anton Dubovik <anton_dubovik@epam.com>
2024-09-26 14:38:25 +0100

    fix: minor fix

--- aidial_adapter_openai/utils/merge_chunks.py
+++ aidial_adapter_openai/utils/merge_chunks.py
@@ -1,20 +1,31 @@
+from typing import TypeVar
+
 from aidial_sdk.utils.merge_chunks import merge
 
+_Chunk = TypeVar("_Chunk", bound=dict)
 
-def merge_chunks(target: dict, source: dict) -> dict:
+
+def merge_chunks(*chunks: _Chunk) -> _Chunk:
     """
     The recursive merging procedure that avoids merging top-level atomic fields
     (e.g. "id", "created", "model", "object", "system_fingerprint") and
     instead chooses an _override_ merging strategy for such fields.
-
-    Non-atomic field (e.g. "choice", "usage") are merged following
-    the standard merging procedure.
+    Non-atomic fields (e.g. "choice", "usage") are merged following
+    the standard recursive merging procedure.
     """
-    source = source.copy()
 
-    for key, value in list(source.items()):
-        if not isinstance(value, (list, dict)) and value is not None:
-            target[key] = value
-            del source[key]
+    assert len(chunks) > 0, "At least one chunk must be provided"
 
-    return merge(target, source)
+    target = chunks[0]
+
+    def _generator():
+        yield target
+        for source in chunks[1:]:
+            source = source.copy()
+            for key, value in list(source.items()):
+                if not isinstance(value, (list, dict)) and value is not None:
+                    target[key] = value
+                    del source[key]
+            yield source
+
+    return merge(*list(_generator()))
