0b08f3665daab009295298fbcb66a467fceb74d9
Roman Romanov <roman_romanov2@epam.com>
2024-11-27 10:47:32 +0200

    Turn off telemetry for test app instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from aidial_adapter_openai.utils.request import get_app_config
 
 @pytest_asyncio.fixture
 def _app_instance():
-    return create_app()
+    return create_app(to_init_telemetry=False)
 
 
 @pytest_asyncio.fixture
