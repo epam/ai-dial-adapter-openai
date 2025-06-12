4cb1b9931e9d6e5dd05bcd77baf30d8fd13c6bbe
Anton Dubovik <anton_dubovik@epam.com>
2025-06-12 14:54:28 +0100

    feat: improve deprecation message

--- aidial_adapter_openai/configuration/deprecations.py
+++ aidial_adapter_openai/configuration/deprecations.py
@@ -2,11 +2,21 @@ import os
 
 from aidial_adapter_openai.utils.log_config import logger
 
-_gpt4v_is_retired = "GPT-4 Vision models has been deprecated both in Azure OpenAI and on OpenAI Platform"
+_gpt4_deprecation_message = "GPT-4 Vision models has been deprecated both in Azure OpenAI and on OpenAI Platform."
 
 _deprecated_env_vars = {
-    "GPT4_VISION_DEPLOYMENTS": _gpt4v_is_retired,
-    "GPT4_VISION_MAX_TOKENS": _gpt4v_is_retired,
+    "GPT4_VISION_DEPLOYMENTS": (
+        f"{_gpt4_deprecation_message} "
+        """
+In the unlikely case that the deployments declared under this variable are still in use:
+1. move them to the GPT4O_DEPLOYMENTS variable,
+2. map the deployments to "gpt-4" in the TIKTOKEN_MODEL_MAPPING variable,
+3. remove the deprecated variable.""".strip()
+    ),
+    "GPT4_VISION_MAX_TOKENS": (
+        f"{_gpt4_deprecation_message} "
+        f"The variable is of no use, it could be safely removed."
+    ),
 }
 
 
@@ -14,6 +24,5 @@ def check_deprecated_env_vars():
     for name, message in _deprecated_env_vars.items():
         if os.getenv(name) is not None:
             logger.warning(
-                f"Environment variable {name!r} is deprecated, since {message}. "
-                "The variable could be safely removed from the environment."
+                f"Environment variable {name!r} is deprecated. {message}"
             )
