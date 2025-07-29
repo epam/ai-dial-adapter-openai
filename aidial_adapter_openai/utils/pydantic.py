8078e3efc7e7cebdecfd7a24c27aae06a6741118
Anton Dubovik <anton_dubovik@epam.com>
2025-07-29 11:25:44 +0100

    fix: simplified DeploymentAPIType

--- aidial_adapter_openai/utils/pydantic.py
+++ aidial_adapter_openai/utils/pydantic.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class ExtraAllowedModel(BaseModel):
     class Config:
         extra = "allow"
+
+
+class ExtraForbidModel(BaseModel):
+    class Config:
+        extra = "forbid"
