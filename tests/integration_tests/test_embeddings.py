e3a60d34604eedcb7d225952c25c5b2654161c85
Anton Dubovik <anton_dubovik@epam.com>
2025-07-31 12:50:49 +0100

    feat: add test for multi-modal embeddings

--- tests/integration_tests/test_embeddings.py
+++ tests/integration_tests/test_embeddings.py
@@ -3,28 +3,36 @@ from typing import List
 import openai
 import pytest
 
+from aidial_adapter_openai.configuration.app_config import ApplicationConfig
 from tests.integration_tests.base import (
     DeploymentConfig,
     EmbeddingsDeploymentType,
     sanitize_id_part,
 )
-from tests.integration_tests.constants import TEST_DEPLOYMENTS_CONFIG
+from tests.integration_tests.constants import (
+    SAMPLE_DOG_RESOURCE,
+    TEST_DEPLOYMENTS_CONFIG,
+)
 
 D = DeploymentConfig[EmbeddingsDeploymentType]
 
 _deployments: List[D] = list(TEST_DEPLOYMENTS_CONFIG.embedding_deployments)
 
 
-def _display_config(deployment: D) -> str:
-    upstream_idx = deployment.upstream_idx
-    parts = [
-        sanitize_id_part(deployment.id_),
-        *([] if upstream_idx is None else [f"upstream:{upstream_idx}"]),
-    ]
-    return "/".join(parts)
+@pytest.fixture
+def app_config() -> ApplicationConfig:
+    return TEST_DEPLOYMENTS_CONFIG.app_config
 
 
 if _deployments:
+
+    def _display_config(deployment: D) -> str:
+        upstream_idx = deployment.upstream_idx
+        parts = [
+            sanitize_id_part(deployment.id_),
+            *([] if upstream_idx is None else [f"upstream:{upstream_idx}"]),
+        ]
+        return "/".join(parts)
 
     @pytest.fixture(params=_deployments, ids=_display_config)
     def deployment(request) -> D:
@@ -61,3 +69,25 @@ async def test_embeddings_two_text_inputs(
         model=model_name, input=["cat", "fish"]
     )
     assert len(response.data) == 2
+
+
+@pytest.fixture
+def image_input_supported(
+    model_name: str, app_config: ApplicationConfig
+) -> bool:
+    if model_name in app_config.AZURE_AI_VISION_DEPLOYMENTS:
+        return True
+    pytest.skip("Embeddings doesn't support images")
+
+
+async def test_embeddings_image_input(
+    image_input_supported, model_name: str, client: openai.AsyncAzureOpenAI
+):
+    resource = SAMPLE_DOG_RESOURCE
+    image_attachment = {"type": resource.type, "data": resource.data_base64}
+    response = await client.embeddings.create(
+        model=model_name,
+        input=[],
+        extra_body={"custom_input": [image_attachment]},
+    )
+    assert len(response.data) == 1
