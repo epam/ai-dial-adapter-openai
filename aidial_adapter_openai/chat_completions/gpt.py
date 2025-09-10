8a160dc0257dc221435458bf5be9a3fac839f01e
Anton Dubovik <anton_dubovik@epam.com>
2025-09-10 17:11:16 +0200

    fix: offload CPU-heavy tasks to a thread (#290)

--- aidial_adapter_openai/chat_completions/gpt.py
+++ aidial_adapter_openai/chat_completions/gpt.py
@@ -1,4 +1,4 @@
-from typing import AsyncIterator, Callable, List, Mapping, Tuple
+from typing import AsyncIterator, Callable, Coroutine, List, Mapping, Tuple
 
 from aidial_sdk.exceptions import InvalidRequestError
 from openai import AsyncAzureOpenAI, AsyncOpenAI, AsyncStream
@@ -27,19 +27,19 @@ from aidial_adapter_openai.utils.truncate_prompt import (
 )
 
 
-def multi_modal_truncate_prompt(
+async def multi_modal_truncate_prompt(
     request: dict,
     messages: List[MultiModalMessage],
     max_prompt_tokens: int,
     tokenizer: Tokenizer,
 ) -> Tuple[List[MultiModalMessage], DiscardedMessages, TruncatedTokens]:
-    return truncate_prompt(
+    return await truncate_prompt(
         messages=messages,
         message_tokens=tokenizer.tokenize_request_message,
         is_system_message=lambda message: message.raw_message["role"]
         == "system",
         max_prompt_tokens=max_prompt_tokens,
-        initial_prompt_tokens=tokenizer.tokenize_request(request, []),
+        initial_prompt_tokens=await tokenizer.tokenize_request(request, []),
     )
 
 
@@ -61,16 +61,16 @@ def _extract_max_prompt_tokens(request: dict) -> int | None:
     return max_prompt_tokens
 
 
-def _truncate_messages(
+async def _truncate_messages(
     request: dict, messages: List[MultiModalMessage], tokenizer: Tokenizer
 ) -> Tuple[
     List[MultiModalMessage],
     DiscardedMessages | None,
-    Callable[[], TruncatedTokens],
+    Callable[[], Coroutine[None, None, TruncatedTokens]],
 ]:
     if (max_prompt_tokens := _extract_max_prompt_tokens(request)) is not None:
         messages, discarded_indices, prompt_tokens = (
-            multi_modal_truncate_prompt(
+            await multi_modal_truncate_prompt(
                 request=request,
                 messages=messages,
                 max_prompt_tokens=max_prompt_tokens,
@@ -83,11 +83,14 @@ def _truncate_messages(
             f"discarded messages indices: {discarded_indices}"
         )
 
-        return messages, discarded_indices, lambda: prompt_tokens
+        async def get_prompt_tokens() -> int:
+            return prompt_tokens
+
+        return messages, discarded_indices, get_prompt_tokens
     else:
 
-        def get_prompt_tokens() -> int:
-            prompt_tokens = tokenizer.tokenize_request(request, messages)
+        async def get_prompt_tokens() -> int:
+            prompt_tokens = await tokenizer.tokenize_request(request, messages)
             logger.debug(f"estimated prompt tokens: {prompt_tokens}")
             return prompt_tokens
 
@@ -112,7 +115,7 @@ async def chat_completion(
     ).transform_messages(messages)
 
     multi_modal_messages, discarded_messages, get_prompt_tokens = (
-        _truncate_messages(request, multi_modal_messages, tokenizer)
+        await _truncate_messages(request, multi_modal_messages, tokenizer)
     )
 
     request["messages"] = [m.raw_message for m in multi_modal_messages]
@@ -122,7 +125,7 @@ async def chat_completion(
     )
 
     if isinstance(response, AsyncStream):
-        response_headers = get_response_headers_for_caching(
+        response_headers = await get_response_headers_for_caching(
             request_headers=request_headers,
             request_body=request,
             get_request_tokens=get_prompt_tokens,
@@ -148,11 +151,13 @@ async def chat_completion(
         if usage := response.usage:
             actual_prompt_tokens = usage.prompt_tokens
 
-        response_headers = get_response_headers_for_caching(
+        async def get_request_tokens():
+            return actual_prompt_tokens or await get_prompt_tokens()
+
+        response_headers = await get_response_headers_for_caching(
             request_headers=request_headers,
             request_body=request,
-            get_request_tokens=lambda: actual_prompt_tokens
-            or get_prompt_tokens(),
+            get_request_tokens=get_request_tokens,
         )
 
         debug_print("response", body)
